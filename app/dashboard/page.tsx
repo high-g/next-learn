5a2c5f6b4d3e2e668dcab2b8e39b3df0c8a8f28f
high-g <altema1217bbfumi@live.jp>
2023-12-21 12:03:11 +0900

    Refactor dashboard page and revenue chart components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <p>Dashboard page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Total Revenue" value={revenue} /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
